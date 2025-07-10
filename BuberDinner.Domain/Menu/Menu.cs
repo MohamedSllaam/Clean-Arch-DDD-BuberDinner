c5284c3756132188b500c13ff08b96354aead18d
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2025-07-10 19:29:49 +0300

    Add Menu Model

---
+++ BuberDinner.Domain/Menu/Menu.cs
@@ -0,0 +1,42 @@
+using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Menu.Entities;
+using BuberDinner.Domain.Menu.ValueObjects;
+
+namespace BuberDinner.Domain.Menu
+{
+    public class Menu : AggregateRoot<MenuId>
+    {
+        private readonly List<MenuSection> _sections = new();
+        public string Name { get; }
+        public string Description { get; }
+        public float AverageRating { get; }
+
+        public IReadOnlyList<MenuSection> Sections => _sections.AsReadOnly();
+
+
+        private Menu(
+            MenuId id,
+            string name,
+            string description,
+            float averageRating) : base(id)
+        {
+            Name = name;
+            Description = description;
+            AverageRating = averageRating;
+        }
+        public static Menu Create(
+            string name,
+            string description,
+            float averageRating)
+        {
+            return new Menu(
+                MenuId.CreateUnique(),
+                name,
+                description,
+                averageRating);
+        }
+        // Additional methods and properties can be added here
+
+
+    }
+}
