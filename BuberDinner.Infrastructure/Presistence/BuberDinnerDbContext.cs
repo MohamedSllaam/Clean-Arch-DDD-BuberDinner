2a0c97f29aeb151f99b61ed4616dccb0664150d5
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2025-07-17 21:10:44 +0300

    Add BuberDinnerDbContext

---
+++ BuberDinner.Infrastructure/Presistence/BuberDinnerDbContext.cs
@@ -0,0 +1,50 @@
+using BuberDinner.Domain.Bill;
+using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Dinner;
+using BuberDinner.Domain.Guest;
+using BuberDinner.Domain.Host;
+using BuberDinner.Domain.MenuReview;
+using BuberDinner.Domain.Menu;
+using BuberDinner.Domain.User;
+using BuberDinner.Infrastructure.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuberDinner.Infrastructure.Persistence;
+
+public sealed class BuberDinnerDbContext : DbContext
+{
+    private readonly PublishDomainEventsInterceptor _publishDomainEventsInterceptor;
+
+    public BuberDinnerDbContext(
+        DbContextOptions<BuberDinnerDbContext> options,
+        PublishDomainEventsInterceptor publishDomainEventsInterceptor
+    ) : base(options)
+    {
+        _publishDomainEventsInterceptor = publishDomainEventsInterceptor;
+    }
+
+    public DbSet<Bill> Bills { get; set; } = null!;
+    public DbSet<Dinner> Dinners { get; set; } = null!;
+    public DbSet<Guest> Guests { get; set; } = null!;
+    public DbSet<Host> Hosts { get; set; } = null!;
+    public DbSet<Menu> Menus { get; set; } = null!;
+    public DbSet<MenuReview> MenuReviews { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Ignore<List<IDomainEvent>>()
+            .ApplyConfigurationsFromAssembly(typeof(BuberDinnerDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .AddInterceptors(_publishDomainEventsInterceptor);
+
+        base.OnConfiguring(optionsBuilder);
+    }
+}
